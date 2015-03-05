5bb1c56cb793c63df7d9f763d98310180befc8f4
Bharat <bharatappguru@gmail.com>
2015-03-05 19:18:59 +0530

    Multi Language Support To Android App Tutorial

---
+++ src/com/example/androidwithmultiplelanguagesupport/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.androidwithmultiplelanguagesupport;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.lang_setting_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.eng:
+			String languageToLoad = "en"; // your language
+			Locale locale = new Locale(languageToLoad);
+			Locale.setDefault(locale);
+			Configuration config = new Configuration();
+			config.locale = locale;
+			getBaseContext().getResources().updateConfiguration(config,
+					getBaseContext().getResources().getDisplayMetrics());
+			this.setContentView(R.layout.activity_main);
+			break;
+		case R.id.hn:
+			languageToLoad = "hi"; // your language
+			locale = new Locale(languageToLoad);
+			Locale.setDefault(locale);
+			config = new Configuration();
+			config.locale = locale;
+			getBaseContext().getResources().updateConfiguration(config,
+					getBaseContext().getResources().getDisplayMetrics());
+			this.setContentView(R.layout.activity_main);
+			break;
+		case R.id.fr:
+			languageToLoad = "fr"; // your language
+			locale = new Locale(languageToLoad);
+			Locale.setDefault(locale);
+			config = new Configuration();
+			config.locale = locale;
+			getBaseContext().getResources().updateConfiguration(config,
+					getBaseContext().getResources().getDisplayMetrics());
+			this.setContentView(R.layout.activity_main);
+			break;
+		case R.id.de:
+			languageToLoad = "de"; // your language
+			locale = new Locale(languageToLoad);
+			Locale.setDefault(locale);
+			config = new Configuration();
+			config.locale = locale;
+			getBaseContext().getResources().updateConfiguration(config,
+					getBaseContext().getResources().getDisplayMetrics());
+			this.setContentView(R.layout.activity_main);
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
